457c7ad36cb607c6d7bd2fbb36767a4028477afd
Paula Grangeiro <contato@paulagrangeiro.com.br>
2017-03-19 20:06:34 -0300

    Add chat models

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room)
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
